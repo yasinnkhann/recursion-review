cc796c0eaca4b2c08dbb8ce4f3300b3a625e13f4
Yasin Khan <yasinkhan500@gmail.com>
2021-11-08 14:26:35 -0800

    solved getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,30 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var result = [];
+  var bodyTag = document.body;
+
+  var hasClass = function(elem) {
+    if (elem.classList !== undefined) {
+      if (elem.classList.contains(className)) {
+        result.push(elem);
+      }
+    }
+
+    elem.childNodes.forEach(function(childNode) {
+      hasClass(childNode);
+    });
+  }
+
+  hasClass(bodyTag);
+  return result;
 };
+
+// Have to use: childNodes, classList, contains,
+
+// Input: className (string)
+// Output: collection of elements (array)
+// Constraints: Can't use document.getElementsByClassName(className)
+// Edge Case: Elements that don't have class names --> don't push into our array
