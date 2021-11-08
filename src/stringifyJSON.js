6915facc159813e4b137fd564d041f42bf269086
Yasin Khan <yasinkhan500@gmail.com>
2021-11-08 15:51:36 -0800

    implemented array case

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,34 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // Primitives
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString();
+  }
+
+  if (obj === null) {
+    return 'null';
+  }
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  // Collections
+  if (Array.isArray(obj)) {
+    var result = '';
+
+    // if (obj.length === 0) {
+    //   return '[]';
+    // }
+    obj.forEach(function(item) {
+      result += stringifyJSON(item) + ',';
+    })
+    return '[' + result.slice(0, result.length - 1) + ']';
+  }
 };
+
+// Input: obj
+// Output: A stringified version of the input object
+// Constraints: Can't use the native JSON method
+// Edge Cases: If obj is a primitive, then don't use recursion
